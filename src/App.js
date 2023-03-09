931579c21f19a9b0ffb774b1a4d67d69ea8ed517
Paul Nix <kenpopaul@yahoo.co.uk>
2023-03-09 21:35:49 +0000

    added in CSS and search.svg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 const API_URL = 'http://www.omdbapi.com?apikey=a44ac51f';
 
 
 const App = () => {
+
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+}
+
+useEffect(() => {
+    searchMovies('Spiderman');
+}, []);
+
     return (
         <h1>App</h1>
     );
