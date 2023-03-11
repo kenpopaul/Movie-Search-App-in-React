453583cc49e64a581d2b3048e06df4282bff6ed5
Paul Nix <kenpopaul@yahoo.co.uk>
2023-03-11 06:59:11 +0000

    added MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { useEffect } from 'react';
+import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=a44ac51f';
+
+const movie1 = 
+    {
+        "Title": "Spiderman the Verse",
+        "Year": "2019–",
+        "imdbID": "tt12122034",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
+    }
+
+
 
 
 const App = () => {
@@ -30,7 +42,12 @@ useEffect(() => {
                 <img
                     src={SearchIcon}
                     alt="search"
+                    onClick={() => {}}
                 />
+            </div>
+
+            <div className="container">
+               <MovieCard movie1={movie1}/>
             </div>
         </div>
     );
